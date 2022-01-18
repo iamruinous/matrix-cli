8caef5f41a5c197fe63cc4c359b757510e058d47
Jade Meskill <jade.meskill@gmail.com>
2022-01-18 10:12:52 -0700

    add session handling, requires unreleased matrix-sdk for now

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,15 @@ struct Cli {
 
     /// Your matrix username
     #[structopt(short, long, env = "MATRIX_CLI_USERNAME")]
-    username: String,
+    username: Option<String>,
 
     /// Your matrix password
     #[structopt(short, long, env = "MATRIX_CLI_PASSWORD")]
-    password: String,
+    password: Option<String>,
+
+    /// Use or store the session information here
+    #[structopt(short, long, env = "MATRIX_CLI_SESSION_FILE")]
+    session_file: Option<PathBuf>,
 
     #[structopt(subcommand)]
     subcommands: Option<MatrixCli>,
@@ -38,6 +42,8 @@ enum MatrixCli {
 
 #[derive(StructOpt, Debug)]
 enum User {
+    /// Gets the users display name
+    GetDisplayName {},
     /// Set the users display name
     SetDisplayName {
         #[structopt(name = "NAME")]
@@ -61,25 +67,53 @@ async fn main() -> Result<()> {
     let args = Cli::from_args();
     let homeserver_url_str = args.homeserver_url;
     let homeserver_url = Url::parse(&homeserver_url_str).unwrap();
-    let username = args.username;
-    let password = args.password;
-    let client = Client::new(homeserver_url).unwrap();
-    let response = client
-        .login(&username, &password, None, Some("matrix-cli"))
-        .await
-        .unwrap();
 
-    println!(
-        "Logged in as {}, got device_id {} and access_token {}",
-        response.user_id, response.device_id, response.access_token
-    );
+    let session_file = args.session_file;
+    let session_file_exists = match &session_file {
+        None => false,
+        Some(f) => f.exists(),
+    };
+
+    let client = Client::new(homeserver_url).unwrap();
+    match session_file_exists {
+        false => {
+            let username = args.username.unwrap();
+            let password = args.password.unwrap();
+            let _response = client
+                .login(&username, &password, None, Some("matrix-cli"))
+                .await?;
+
+            // Only write the session if the session_file is specified
+            if session_file.is_some() {
+                let session_path = File::create(session_file.unwrap())?;
+                let session = client.session().await.unwrap();
+
+                serde_json::to_writer(session_path, &session)?;
+            }
+        }
+        true => {
+            let session_path = File::open(session_file.unwrap())?;
+            let session: matrix_sdk::Session =
+                serde_json::from_reader(session_path)?;
+            client.restore_login(session).await?;
+        }
+    };
+
     if let Some(scmd) = args.subcommands {
         match scmd {
             MatrixCli::User { commands } => {
                 if let Some(cmd) = commands {
                     match cmd {
+                        User::GetDisplayName {} => {
+                            match client.display_name().await? {
+                                None => println!("Display Name Not Set"),
+                                Some(display_name) => {
+                                    println!("{}", display_name);
+                                }
+                            };
+                        }
                         User::SetDisplayName { name } => {
-                            client.set_display_name(Some(&name)).await?
+                            client.set_display_name(Some(&name)).await?;
                         }
                         User::SetAvatar { file } => {
                             let guess = mime_guess::from_path(file.as_path());
